73e99f3702d7f76d484e22243396c78c444a1260
Maisie <maisiesadler@hotmail.com>
2022-03-22 06:50:41 +0000

    undo

--- run-redoc.sh
+++ run-redoc.sh
@@ -1,7 +1,7 @@
 cd yamltojson
-npm run start ../identity.yaml ../identity.json
+npm run start ../dist.yaml ../dist.json
 cd ../
 
 dotnet run --project src/OpenApiSpecGeneration.ApiGeneration.Console \
-    generate-mock -i identity.json -o example-2/generated -n Example
+    generate-mock -i dist.json -o example-2/generated -n Example
 
